b63e39474848670f4a1ffa92f849b94d269e78f6
erkam <101091269+B-erkamklnc@users.noreply.github.com>
2025-02-16 08:15:38 +0300

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -1,118 +1,68 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity 0.8.26;
 
-contract KapsulVoting {
+interface IUniswapV2Pair {
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+}
 
-    uint8 projectIDCount = 1;
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);   
+}
 
-    struct Project {
-        uint8 id;
-        string projectName;
-        address[] teamWallets;
-        uint8 voteCount;
+contract TokenLock {
+    uint256 ethAmount;
+    uint256 timeUp = block.timestamp + 31536000; // 1 yıl
+    address tokenAddress;
+    address lp;
+    address owner;
+    address WETH = address(0x0);
+
+    constructor(uint256 _ethAmount, address _tokenAddress, address _lp, address _owner) {
+        ethAmount = _ethAmount;
+        tokenAddress = _tokenAddress;
+        lp = _lp;
+        owner = _owner;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function raiseStaking(uint256 _amount) public {
+        IERC20 _token = IERC20(WETH);
+        _token.transferFrom(owner, address(this), _amount);
+
+        ethAmount += _amount;
+    }
+
+    function getEthAmount() public view returns(uint256) {
+        return ethAmount;
     }
 
 
-    event ProjectCreated(
-        uint8 indexed projectId,
-        string projectName,
-        address[] teamWallets
-    );
-
-    event VoteCasted(
-        address indexed voter,
-        uint8 indexed projectId,
-        uint8 newVoteCount
-    );
-
-    event ProjectUpdated(
-        uint8 indexed projectId,
-        string newProjectName,
-        address[] newTeamWallets
-    );
-
-    event WalletAddedToGroup(
-        address indexed wallet,
-        uint8 indexed projectId
-    );
-
-    mapping(uint8 => Project) projectDetail;
-    mapping(string => uint8) projectIDByName;
-
-    mapping(address => uint8) walletGroup;
-    mapping(address => bool) isVoted;
-
-    function addProject(string memory _projectName, address[] memory _teamWallets) public {
-        require(projectIDByName[_projectName] == 0, "Project already exists");
-
-        Project memory _newProject = Project({
-            id: projectIDCount,
-            projectName: _projectName,
-            teamWallets: _teamWallets,
-            voteCount: 0
-        });
-
-        projectDetail[projectIDCount] = _newProject;
-        projectIDByName[_projectName] = projectIDCount;
-
-        addToWalletGroup(projectIDCount, _teamWallets);
-
-        emit ProjectCreated(projectIDCount, _projectName, _teamWallets);
-
-        projectIDCount++;
+    function rugPullInsurance() public view {
+        IUniswapV2Pair pair = IUniswapV2Pair(lp);
+        (uint112 reserve0, uint112 reserve1,) = pair.getReserves();
+        if (!(pair.token0() == WETH && reserve0 > 0) || (pair.token1() == WETH && reserve1 > 0)) {
+            //etkileşime gir!!! adresin blanasına bak supplay ın % de kaçına sahip olduğunu belirle omna göre dağıtım yap.
+        
+}
     }
 
-    function addToWalletGroup(uint8 _projectIDCount, address[] memory _teamWallets) internal {
-        for (uint8 i = 0; i < _teamWallets.length; i++) 
-        {
-            require(walletGroup[_teamWallets[i]] == 0, "Wallet already added to a group");
-            walletGroup[_teamWallets[i]] = _projectIDCount;
-            
-            //Her cüzdan eklendiğinde
-            emit WalletAddedToGroup(_teamWallets[i], _projectIDCount);
-        }
-    }
+    function getStake() public onlyOwner {
+        require(block.timestamp >= timeUp);
 
-    function vote(uint8 _projectID) public {
-        require(_projectID < projectIDCount, "Invalid projed id");
-        require(!isVoted[msg.sender], "This wallet already voted");
-        require(walletGroup[msg.sender] != _projectID, "You can not vote to your own project");
+        IERC20 weth = IERC20(WETH);
+        require(weth.transfer(owner, ethAmount), "Transfer failed");
 
-        Project storage _project = projectDetail[_projectID];
-
-        _project.voteCount++;
-        isVoted[msg.sender] = true;
-
-        //Oy verildiğinde
-        emit VoteCasted(msg.sender, _projectID, _project.voteCount);
-    }
-
-    function updateProject(uint8 _id, string memory _newProjectName, address[] memory _newTeamWallets) public {
-        require(walletGroup[msg.sender] == _id, "You can not change another project details");
-
-        Project storage _newProject = projectDetail[_id];
-
-        _newProject.projectName = _newProjectName;
-        _newProject.teamWallets = _newTeamWallets;
-
-        //Proje güncellendiğinde
-        emit ProjectUpdated(_id, _newProjectName, _newTeamWallets);
-    }
-
-
-    function getWalletGroup(address _wallet) public view returns(uint8) {
-        return walletGroup[_wallet];
-    }
-
-    function getIsVoted(address _wallet) public view returns(bool) {
-        return isVoted[_wallet];
-    }
-
-    function getProjectIDByName(string memory _projectName) public view returns(uint8) {
-        return projectIDByName[_projectName];
-    }
-
-    function getProjectDetail(uint8 _projectID) public view returns(Project memory) {
-        return projectDetail[_projectID];
+        ethAmount = 0; 
     }
 }
